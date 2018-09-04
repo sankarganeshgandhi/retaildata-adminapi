aa61eb0e9c32525c620762240542754409c99e62
Sankarganesh Gandhi <sankarganesh.gandhi@happiestminds.com>
2018-09-04 16:13:07 +0100

    Initial draft API implementation for user management and authentication

--- src/data/ddl/database_ddl.sql
+++ src/data/ddl/database_ddl.sql
@@ -73,19 +73,3 @@ INSERT INTO Address(Line1, Line2, City, Country, Postcode, AddressTypeId, UserTI
     ModifiedDate)
     VALUES ("Address Line1", "Address Line2", "Manchester", "UK", "M210BH",
     1, 1, 2, Now(), Now());
-
-DROP TABLE AuthSession;
-
-CREATE TABLE AuthSession (
-    Id BIGINT(10) NOT NULL AUTO_INCREMENT,
-    UserId INT(10) NOT NULL,
-    Username VARCHAR(30) NOT NULL,
-    SessionId VARCHAR(250) NOT NULL,
-    LoginTime DATE NOT NULL,
-    LogoutTime DATE,
-    StatusId INT(2) NOT NULL,
-    CreatedDate DATE,
-    ModifiedDate DATE,
-    PRIMARY KEY (Id),
-    FOREIGN KEY (StatusId) REFERENCES Status(Id)
-) ENGINE = INNODB;
--- src/main/java/com/sgglabs/retail/AuthSessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sgglabs.retail;
-
-import com.sgglabs.retail.model.AuthSession;
-import com.sgglabs.retail.model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthSessionRepository extends CrudRepository<AuthSession, Long> {
-    public AuthSession findByUserName(final String userName);
-}
